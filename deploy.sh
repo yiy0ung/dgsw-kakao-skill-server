8b8f38306330584719a034a79325c88bebc7b82f
wlsdud2194 <wlsdud2194@naver.com>
2019-12-16 12:15:38 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 DOCKER_KAKAO_SERVER_NAME=kakao-skill-server
 
-EXIST_BLUE=$(docker-compose -p ${DOCKER_KAKAO_SERVER_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_KAKAO_SERVER_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
   echo "Ver.blue up"
@@ -19,5 +19,3 @@ else
 
   sudo docker-compose -p ${DOCKER_KAKAO_SERVER_NAME}-blue -f docker-compose.blue.yml down
 fi
-
-
